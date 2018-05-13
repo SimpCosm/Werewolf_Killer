620ddd23adc61eab975274bb431290839680a973
pkuzgy <zhaoguangyang@pku.edu.cn>
2018-05-13 09:14:08 +0800

    page finished

--- HackthonWeb/web/build/lynchRevealScreen.js
+++ HackthonWeb/web/build/lynchRevealScreen.js
@@ -1,12 +1,30 @@
 ww.lynchRevealScreen = {};
 
-$(function() {
+$(function () {
 
-    ww.lynchRevealScreen.showLynchedPlayer = function(lynchPlayer) {
+    ww.lynchRevealScreen.showLynchedPlayer = function (lynchPlayer) {
         $("#lynchRevealText").html(wwhtml.getDeathText(lynchPlayer));
+        setTimeout(function () {
+            var gameOver = wwgame.getGameOver();
+
+            if (gameOver) {
+                // Done!
+                ww.gameOverScreen.showGameOver(gameOver);
+                changeScreens("#gameOverScreen");
+            }
+            else {
+                // Setup the first player's lock screen, game continues...
+                ww.duskWolfScreen.resetToFirstPlayer();
+                ww.duskWolfScreen.cmdPlayerActions();
+                //ww.duskLockedScreen.showCurrentPlayer();
+
+                changeScreens("#duskWolfScreen");
+            }
+        }, 10000);
+
     };
 
-    $("#lynchRevealContinue").click(function() {
+    $("#lynchRevealContinue").click(function () {
         var gameOver = wwgame.getGameOver();
 
         if (gameOver) {
